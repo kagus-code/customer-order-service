20febf3736c3072f9656c9208ca804afe1c1b825
kagus-code <eston.kagwima@actserv.co.ke>
2025-02-06 12:02:49 +0300

    chore(Production): Config Production files
    -Add whitenoise for the static files
    -configure staticfiles volumes
    -config prod.py

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,7 +6,9 @@ set -e
 echo "Running migrations..."
 python manage.py migrate --noinput
 
+# Collect static files
+python manage.py collectstatic --noinput
 
-echo "Starting Gunicorn server..."
+echo "Starting  server..."
 python manage.py runserver 0.0.0.0:3016
 
